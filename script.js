89b2473c66e3ee2b7136e80877e772de5ead2c63
adibazufarova <adibazufarova@gmail.com>
2022-02-05 23:53:38 +0500

    initial

---
+++ script.js
@@ -0,0 +1,11 @@
+function applicationBtn() {
+  window.location.assign("https://elegant-euclid-16ccaf.netlify.app/");
+}
+
+function detailedBtn1() {
+  window.location.assign("https://trusting-darwin-33ad1f.netlify.app/");
+}
+
+function detailedBtn2() {
+  window.location.assign("https://vigorous-perlman-45db11.netlify.app/");
+}
